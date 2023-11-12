60876fd3819d16861cd9afc07f3a02aabaf6a441
elijah jones <jones.elijah4679@gmail.com>
2023-11-12 13:00:21 -0600

    first

---
+++ Variables_Exercise1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Declare & Initialize
+string name = "Elbow";
+int age = 39;
+char middleInitial = 'S';
+bool isOver18 = true;
+double currentTemp = 77.7;
+decimal currentPrice = 14.57m;
+
+//Writing the varibles to the console 
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18e: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
+
